e835e8672c2ea26ad5b33807d9e63683d070e77f
Carlos Jimenez Fabra <cjimenez@aula-digital.com>
2018-05-28 21:53:06 +0200

    Adds .sln, RestApiEngine Package and TestConsoleApp

---
+++ RestApiEngine/RestEngine.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiEngine
+{
+    public class RestEngine : HttpClient
+    {
+        private string BaseUrl { get; set; }
+        private string Accept { get; set; }
+        public List<string> GetUriParams { get; private set; } = new List<string>();
+        public Dictionary<string, string> QueryParams { get; private set; } = new Dictionary<string, string>();
+        public StringContent StringBodyContent { get; private set; }
+
+        public RestEngine()
+        {
+
+        }
+
+        public RestEngine(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+        public RestEngine(string baseUrl, string accept)
+        {
+            BaseUrl = baseUrl;
+            AddAccept(accept);
+        }
+
+        public RestEngine AddAccept(string accept)
+        {
+            Accept = accept;
+            DefaultRequestHeaders.Accept.Clear();
+            DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(accept));
+            return this;
+        }
+
+        public RestEngine AddHeader(string key, string value)
+        {
+            DefaultRequestHeaders.Add(key, value);
+            return this;
+        }
+
+        public RestEngine AddUriParam(string param)
+        {
+            GetUriParams.Add(param);
+            return this;
+        }
+
+        public RestEngine AddQueryParam(string key, string value)
+        {
+            if(!QueryParams.ContainsKey(key))
+            {
+                QueryParams.Add(key, value);
+            }
+            return this;
+        }
+
+        public RestEngine AddBearerAuthentication(string token)
+        {
+            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            return this;
+        }
+
+        public RestEngine AddCustomAuthentication(string scheme, string token)
+        {
+            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme, token);
+            return this;
+        }
+
+        public RestEngine AddBodyString(string bodyContent, Encoding encoding, string contentType)
+        {
+            StringBodyContent = new StringContent(bodyContent, encoding, contentType);
+            return this;
+        }
+
+        public RestEngine ClearAccept()
+        {
+            Accept = null;
+            DefaultRequestHeaders.Accept.Clear();
+            return this;
+        }
+
+        public RestEngine ClearHeader(string key)
+        {
+            if (DefaultRequestHeaders.Contains(key))
+            {
+                DefaultRequestHeaders.Remove(key);
+            }
+            return this;
+        }
+
+        public RestEngine ClearHeaders()
+        {
+            Accept = null;
+            DefaultRequestHeaders.Clear();
+            return this;
+        }
+
+        public RestEngine ClearBaseUrl()
+        {
+            BaseUrl = null;
+            return this;
+        }
+
+        public RestEngine ClearUriParam(string key)
+        {
+            GetUriParams.Remove(key);
+            return this;
+        }
+
+        public RestEngine ClearQueryParam(string key)
+        {
+            QueryParams.Remove(key);
+            return this;
+        }
+
+        public RestEngine ClearUriParams()
+        {
+            GetUriParams = new List<string>();
+            return this;
+        }
+
+        public RestEngine ClearQueryParams()
+        {
+            QueryParams = new Dictionary<string, string>();
+            return this;
+        }
+
+        public async Task<HttpResponseMessage> ProcessGetAsync(string path)
+        {
+            ValidateCall();
+
+            var result = await GetAsync(GetCompleteUrl(path));
+
+            return result;
+        }
+
+        public HttpResponseMessage ProcessGetSync(string path)
+        {
+            ValidateCall();
+
+            return GetAsync(GetCompleteUrl(path)).Result;
+        }
+
+        public async Task<HttpResponseMessage> ProcessGetAsync()
+        {
+            ValidateCall();
+
+            var result = await GetAsync(GetCompleteUrl());
+
+            return result;
+        }
+
+        public HttpResponseMessage ProcessGetSync()
+        {
+            ValidateCall();
+
+            return GetAsync(GetCompleteUrl()).Result;
+        }
+
+        public async Task<HttpResponseMessage> ProcessPostStringAsync()
+        {
+            var result = await PostAsync(GetCompleteUrl(), StringBodyContent);
+            return result;
+        }
+
+        public HttpResponseMessage ProcessPostStringSync()
+        {
+            return PostAsync(GetCompleteUrl(), StringBodyContent).Result;
+        }
+
+        public async Task<HttpResponseMessage> ProcessPostStringAsync(string path)
+        {
+            var result = await PostAsync(GetCompleteUrl(path), StringBodyContent);
+            return result;
+        }
+
+        public HttpResponseMessage ProcessPostStringSync(string path)
+        {
+            return PostAsync(GetCompleteUrl(path), StringBodyContent).Result;
+        }
+
+        private string GetCompleteUrl()
+        {
+            var completeUrl = GetCompleteUrl(GetUriParams);
+            if (QueryParams.Count > 0)
+            {
+                AddQueryParamsToUrl(QueryParams, ref completeUrl);
+            }
+            return completeUrl;
+        }
+
+        private string GetCompleteUrl(string path)
+        {
+            string[] pathParts = path.Split('/');
+            return GetCompleteUrl(pathParts);
+        }
+
+        private string GetCompleteUrl(IEnumerable<string> uriParams)
+        {
+            var completeUrl = BaseUrl;
+            foreach (var part in uriParams)
+            {
+                if (!completeUrl.EndsWith('/'))
+                {
+                    completeUrl = completeUrl + "/";
+                }
+                if (!GetUriParams.Contains(part))
+                {
+                    GetUriParams.Add(part);
+                }
+                completeUrl = completeUrl + part;
+            }
+
+            return completeUrl;
+        }
+
+        private void AddQueryParamsToUrl(IDictionary<string, string> queryParams, ref string url)
+        {
+            foreach(var param in queryParams)
+            {
+                if (!url.Contains("?"))
+                {
+                    url = url + "?";
+                }
+                else
+                {
+                    url = url + "&";
+                }
+                url = url + param.Key + "=" + param.Value;
+            }
+        }
+
+        private bool ValidateBaseUrl() => !string.IsNullOrEmpty(BaseUrl);
+
+        private void ValidateCall()
+        {
+            if (!ValidateBaseUrl())
+            {
+                throw new ArgumentNullException("BaseUrl cannot be null");
+            }
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using RestApiEngine;
+using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var engine = new RestEngine("https://reqres.in/api/", "application/json")
+                .AddHeader("User-Agent", "RestEngine .NET");
+            var task = engine.ProcessGetSync("users");
+            var result = task.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+
+
+            engine.ClearUriParams()
+                .ClearHeaders()
+                .AddAccept("application/json")
+                .AddHeader("User-Agent", ".NET Foundation Repository Reporter")
+                .AddUriParam("users")
+                .AddQueryParam("page", "3");
+            var task2 = engine.ProcessGetSync();
+            var result2 = task.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result2);
+
+            engine.AddUriParam("1");
+            var task3 = engine.ProcessGetSync();
+            var result3 = task.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result3);
+
+            engine.AddUriParam("2");
+            task3 = engine.ProcessGetSync();
+            result3 = task.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result3);
+
+            engine.ClearUriParams().AddUriParam("users").AddQueryParam("page", "2");
+            task3 = engine.ProcessGetSync();
+            result3 = task.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result3);
+
+            Console.WriteLine("Press Any Key To Finish");
+            Console.ReadLine();
+        }
+    }
+}
